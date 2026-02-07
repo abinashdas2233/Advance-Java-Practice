e7ec170b9ac372928cb9db1f898aefe9aa0d2fb7
abinashdas2233 <abinashdas.8728@gmail.com>
2026-02-07 20:23:17 +0530

    nope

--- Banking_Security_spring_security/src/main/java/com/banksecurity/controller/BankController.java
+++ Banking_Security_spring_security/src/main/java/com/banksecurity/controller/BankController.java
@@ -1,5 +1,6 @@
 package com.banksecurity.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/tran")
 public class BankController {
-	@PostMapping("/deposite")
+	@GetMapping("/deposite")
 	public String deposite() {
 		
-		return String.valueOf(1000);
+		return "Deposite amount-"+String.valueOf(1000);
 		
 	}
+	@GetMapping("/withdraw")
+	public String withDraw() {
+		return "withdraw amount-"+String.valueOf(1000);
+	}
+	@GetMapping("/contactus")
+	public String contactUs() {
+		return "contact us-"+"9668523794";
+	}
+	
+	@GetMapping("/about")
+ public String aboutUs() {
+		return "Hdfc Bank";
+	}
+	
 
 }
